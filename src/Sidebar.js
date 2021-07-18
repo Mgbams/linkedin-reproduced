00a63a1a67294b94b0c0131ade2d98d50972156b
mgbams <cmgbams@gmail.com>
2021-07-18 22:06:27 +0200

    creation of sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "./Sidebar.css";
+import Avatar from "@material-ui/core/Avatar";
 
 function Sidebar() {
   return (
-    <div>
-      <h1>I am a sidebar</h1>
+    <div className="sidebar">
+      <div className="sidebar_top">
+        <img src="" alt="" />
+        <Avatar className="sidebar_avatar" />
+        <h2>Sony Sanga</h2>
+        <h4>sony.sanga@gmail.com</h4>
+      </div>
+
+      <div className="sidebar_stats">
+        <div className="sidebar_stat">
+          <p>Who viewed you</p>
+          <p className="sidebar_statNumber">2,543</p>
+        </div>
+        <div className="sidebar_stat">
+          <p>Views on post</p>
+          <p className="sidebar_statNumber">2,440</p>
+        </div>
+      </div>
+
+      <div className="sibebar_bottom">
+        <p>Recent</p>
+      </div>
     </div>
   );
 }
